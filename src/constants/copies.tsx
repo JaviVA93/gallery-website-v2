478fb1a94750faacbf4989fc7c0d73f3a5a27c9e
Javi Villar <javiroll93@gmail.com>
2023-09-09 11:08:11 +0200

    change copies exportation

--- src/constants/copies.tsx
+++ src/constants/copies.tsx
@@ -1,9 +1,15 @@
-export const COPIES = {
-    HOME: {
-        TITLE: {
-            es: "El arte de Miguel Aguilò",
-            ca: "L'art d'en Miguel Aguilò",
-            en: "The Miguel Aguilò's art",
-        }
+export const HOME_COPIES = {
+    TITLE: {
+        es: "El arte de Miguel Aguilò",
+        ca: "L'art d'en Miguel Aguilò",
+        en: "The Miguel Aguilò's art",
+    }
+}
+
+export const ABOUT_COPIES = {
+    TITLE: {
+        es: "Acerca de Miguel...",
+        ca: "Sobre Miguel...",
+        en: "About Miguel",
     }
 }
