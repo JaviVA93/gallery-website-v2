16db8b5fe5742dadd22aaf6582963dc353848117
Javi Villar <javiroll93@gmail.com>
2023-09-09 10:56:17 +0200

    utility to combine 2 arrays

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,17 +1,28 @@
 export function getRandomElementsFromArray<T>(arr: T[], n: number): T[] {
     if (n >= arr.length) {
         // Return a copy of the entire array if n is greater or equal to the array length
-        return arr.slice(); 
+        return arr.slice();
     }
-    
+
     // Create a copy of the original array
-    const shuffled = arr.slice(); 
+    const shuffled = arr.slice();
     for (let i = shuffled.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         // Swap elements randomly
-        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; 
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
 
     return shuffled.slice(0, n);
 }
 
+export function combineArrays<T, U>(arr1: T[], arr2: U[]): [T, U][] {
+    const combined: [T, U][] = [];
+
+    for (const item1 of arr1) {
+        for (const item2 of arr2) {
+            combined.push([item1, item2]);
+        }
+    }
+
+    return combined;
+}
